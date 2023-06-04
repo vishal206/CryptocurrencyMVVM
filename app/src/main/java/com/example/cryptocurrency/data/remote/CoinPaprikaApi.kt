91a4ec8d6acd166e6b3267456fe1c40123225846
vishal206 <vishal206ani@gmail.com>
2023-06-05 00:20:52 +0530

    Added dependencies

--- app/src/main/java/com/example/cryptocurrency/data/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/example/cryptocurrency/data/remote/CoinPaprikaApi.kt
@@ -1,5 +1,12 @@
 package com.example.cryptocurrency.data.remote
 
+import retrofit2.http.GET
+
 //Define the different functions and routes we want to access from our api
 interface CoinPaprikaApi {
+    //get all coin
+    @GET("/v1/coins")
+    suspend fun getCoins()
+
+    // get detail of coin as per id
 }
