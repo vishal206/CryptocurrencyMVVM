80563c31357b90fc56dcdf30bb24c77d7d170af1
vishal206 <vishal206ani@gmail.com>
2023-06-05 12:25:13 +0530

    Implementing View Model

---
+++ app/src/main/java/com/example/cryptocurrency/presentation/coin_detail/CoinDetailViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.cryptocurrency.presentation.coin_detail
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptocurrency.common.Constants
+import com.example.cryptocurrency.common.Resource
+import com.example.cryptocurrency.domain.use_case.get_coin.GetCoinUseCase
+import com.example.cryptocurrency.domain.use_case.get_coins.GetCoinsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+//Main purpose of ViewModel is to maintain our state
+//Keep the UI state even if we rotate the screen
+@HiltViewModel
+class CoinDetailViewModel @Inject constructor(
+    private val getCoinUseCase: GetCoinUseCase,
+    //save information about the state
+    private val savedStateHandle: SavedStateHandle
+): ViewModel(){
+
+    private val _state = mutableStateOf(CoinDetailState())
+    val state : State<CoinDetailState> =  _state
+
+    init {
+        savedStateHandle.get<String>(Constants.PARAM_COIN_ID)?.let{
+            coinId->getCoin(coinId)
+        }
+    }
+
+    private fun getCoin(coinId:String) {
+        getCoinUseCase(coinId).onEach { result ->
+            when(result){
+                is Resource.Success -> {
+                    _state.value= CoinDetailState(
+                        coin=result.data
+                    )
+                }
+                is Resource.Error -> {
+                    _state.value= CoinDetailState(
+                        error=result.message?:"An unexpected error occured"
+                    )
+                }
+                is Resource.Loading ->{
+                    _state.value = CoinDetailState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
