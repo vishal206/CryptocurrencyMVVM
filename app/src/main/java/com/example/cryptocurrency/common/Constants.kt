b85d976a9acd2b43a2a2f04706049ba5beef092c
vishal206 <vishal206ani@gmail.com>
2023-06-05 11:58:37 +0530

    Setting up dependency injection

--- app/src/main/java/com/example/cryptocurrency/common/Constants.kt
+++ app/src/main/java/com/example/cryptocurrency/common/Constants.kt
@@ -1,4 +1,5 @@
 package com.example.cryptocurrency.common
 
 object Constants {
+    const val BASE_URL="https://api.coinpaprika.com/"
 }
--- app/src/main/java/com/example/cryptocurrency/di/AppModule.kt
+++ app/src/main/java/com/example/cryptocurrency/di/AppModule.kt
@@ -1,5 +1,37 @@
 package com.example.cryptocurrency.di
 
+import com.example.cryptocurrency.common.Constants
+import com.example.cryptocurrency.data.remote.CoinPaprikaApi
+import com.example.cryptocurrency.data.repository.CoinRepositoryImplementation
+import com.example.cryptocurrency.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
+// Dependency injection(di)
+// di helps us to make our dependencies replaceable
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikApi():CoinPaprikaApi{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository{
+        return CoinRepositoryImplementation(api)
+    }
 }
